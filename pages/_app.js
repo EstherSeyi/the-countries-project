50bf0717302ed89b21ccebafac2ad89d2e6d4050
EstherSeyi <sheyiogundijo@gmail.com>
2021-04-01 16:12:57 +0100

    modifies app entry

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { ThemeProvider } from "../context/theme";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 3600000,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
