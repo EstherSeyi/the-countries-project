2f9491642efab32d70b446bd7644abedea415e6d
EstherSeyi <sheyiogundijo@gmail.com>
2021-04-01 16:12:01 +0100

    adds layout component

---
+++ components/layout.jsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+
+import Moon from "./icons/moon";
+
+import { useTheme } from "../context/theme";
+
+export default function Layout({ pageTitle, children }) {
+  const { theme, setTheme } = useTheme();
+
+  function isDark() {
+    return theme === "dark";
+  }
+
+  const handleThemeChange = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="min-h-screen bg-primary text-sm sm:text-base pb-1">
+        <div className="bg-elements py-4 shadow-md ">
+          <div className="flex justify-between w-11/12 mx-auto">
+            <p className="text-secondary">Where in the world?</p>
+            <p
+              className="text-secondary flex items-center cursor-pointer"
+              onClick={handleThemeChange}
+            >
+              <Moon isDark={isDark} />
+              <span className="ml-2">Dark Mode</span>
+            </p>
+          </div>
+        </div>
+        {children}
+      </section>
+    </>
+  );
+}
