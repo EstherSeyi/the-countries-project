bf534925b57306269140f52baf11a25a0044a4f5
EstherSeyi <sheyiogundijo@gmail.com>
2021-04-04 14:12:58 +0100

    modifies single country component

--- pages/country/[pid].jsx
+++ pages/country/[pid].jsx
@@ -1,87 +1,150 @@
+import { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import Layout from "../../components/layout";
 import BackArrow from "../../components/icons/back-arrow";
+import http from "../../utils/http";
 
 export default function Country() {
   const router = useRouter();
   const { pid } = router.query;
+  const [data, setData] = useState({});
 
-  console.log(pid);
+  useEffect(() => {
+    const getX = async () => {
+      try {
+        const { data } = await http().get(`/name/${pid}?fullText=true`);
+
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (pid) {
+      getX();
+    }
+  }, [pid]);
+
+  console.log(data);
+
+  const isLast = (index, data) => index === data?.length - 1;
 
   return (
-    <Layout pageTitle="Countries">
+    <Layout pageTitle={pid}>
       <div className="w-11/12 mx-auto mt-16 mb-16">
-        <button className="bg-elements shadow-lg py-2 px-5 text-secondary flex items-center mb-16">
+        <button
+          className="bg-elements shadow-lg py-2 px-5 text-secondary flex items-center mb-16"
+          onClick={() => router.back()}
+        >
           <BackArrow />
           <span className="ml-2">Back</span>
         </button>
-        <div className="text-secondary flex flex-col sm:flex-row justify-between">
-          <div className="flex-35 mb-8 sm:mb-none sm:mr-10">
-            <img
-              src="https://cutt.ly/2ctCroO"
-              alt="avatar"
-              className="w-full h-96"
-            />
-          </div>
-          <div className="flex-40">
-            <h2 className="text-2xl font-bold mb-6">Belgium</h2>
-            <div className="md:flex justify-between my-auto">
-              <div className="flex-35">
-                <p className="mb-2">
-                  <span>Native Name: </span>
-                  <span className="opacity-50">Belgie</span>
-                </p>
-                <p className="mb-2">
-                  <span>Population: </span>
-                  <span className="opacity-50">11,319,511</span>
-                </p>
-                <p className="mb-2">
-                  <span>Region: </span>
-                  <span className="opacity-50">Europe</span>
-                </p>
-                <p className="mb-2">
-                  <span>Sub Region: </span>
-                  <span className="opacity-50">Western Europe</span>
-                </p>
-                <p className="mb-2">
-                  <span>Capital: </span>
-                  <span className="opacity-50">Brussels</span>
-                </p>
+        {data[0] ? (
+          <div className="text-secondary flex flex-col sm:flex-row justify-between">
+            <div className="flex-35 mb-8 sm:mb-none sm:mr-10">
+              <img
+                src={data ? data[0].flag : "https://cutt.ly/2ctCroO"}
+                alt={`${pid} flag`}
+                className="w-full h-96"
+              />
+            </div>
+            <div className="flex-40">
+              <h2 className="text-2xl font-bold mb-6">{pid}</h2>
+              <div className="md:flex justify-between my-auto">
+                <div className="flex-35">
+                  <p className="mb-2">
+                    <span className="mr-2">Native Name: </span>
+                    <span className="opacity-50">
+                      {data[0]?.nativeName ?? ""}
+                    </span>
+                  </p>
+                  <p className="mb-2">
+                    <span className="mr-2">Population: </span>
+                    <span className="opacity-50">
+                      {data[0]?.population ?? ""}
+                    </span>
+                  </p>
+                  <p className="mb-2">
+                    <span className="mr-2">Region: </span>
+                    <span className="opacity-50">{data[0]?.region ?? ""}</span>
+                  </p>
+                  <p className="mb-2">
+                    <span className="mr-2">Sub Region: </span>
+                    <span className="opacity-50">
+                      {data[0].subregion ?? "N/A"}
+                    </span>
+                  </p>
+                  <p className="mb-2">
+                    <span className="mr-2">Capital: </span>
+                    <span className="opacity-50">
+                      {data[0].capital ?? "N/A"}
+                    </span>
+                  </p>
+                </div>
+                <div className="flex-35">
+                  <p className="mb-2">
+                    <span className="mr-2">Top Level Domain: </span>
+
+                    {data && data[0].topLevelDomain
+                      ? data[0].topLevelDomain.map((item, index) => (
+                          <span className="opacity-50" key={item}>
+                            {item}
+                            {isLast(index, data[0].topLevelDomain) ? "" : ", "}
+                          </span>
+                        ))
+                      : null}
+                  </p>
+                  <p className="mb-2">
+                    <span className="mr-2">Currencies: </span>
+
+                    {data && data[0].currencies
+                      ? data[0].currencies.map((item, index) => {
+                          return (
+                            <span key={item.code} className="opacity-50">
+                              {item.name}{" "}
+                              {isLast(index, data[0].currencies) ? "" : ", "}
+                            </span>
+                          );
+                        })
+                      : null}
+                  </p>
+                  <p className="mb-2">
+                    <span className="mr-2">Languages: </span>
+                    <span className="opacity-50">
+                      {data && data[0].languages
+                        ? data[0].languages.map((item, index) => (
+                            <Fragment key={index}>
+                              {item.name}
+                              {isLast(index, data[0].languages) ? "" : ", "}
+                            </Fragment>
+                          ))
+                        : null}
+                    </span>
+                  </p>
+                </div>
               </div>
-              <div className="flex-35">
-                <p className="mb-2">
-                  <span>Top Level Domain: </span>
-                  <span className="opacity-50">.be</span>
-                </p>
-                <p className="mb-2">
-                  <span>Currencies: </span>
-                  <span className="opacity-50">Euro</span>
-                </p>
-                <p className="mb-2">
-                  <span>Languages: </span>
-                  <span className="opacity-50">Dutch, French, German</span>
+              <div className="text-secondary mt-8">
+                <p className="flex items-center">
+                  <span className="mr-4">Border Countries: </span>
+                  <span className="flex flex-wrap">
+                    {data && data[0].borders?.length ? (
+                      data[0].borders.map((item, index) => (
+                        <span
+                          key={`${index}_${item}`}
+                          className="shadow py-2 px-4 mb-2 bg-elements mr-2"
+                        >
+                          {item}
+                        </span>
+                      ))
+                    ) : (
+                      <em className="opacity-50">No border countries</em>
+                    )}
+                  </span>
                 </p>
               </div>
             </div>
-            <div className="text-secondary mt-8">
-              <p className="flex items-center">
-                <span className="mr-4">Border Countries: </span>
-                <span className="flex flex-wrap">
-                  <span className="shadow py-2 px-4 mb-2 bg-elements mr-2">
-                    France
-                  </span>
-                  <span className="shadow py-2 px-4 bg-elements mb-2 mr-2">
-                    Germany
-                  </span>
-                  <span className="shadow py-2 px-4 bg-elements mb-2 mr-2">
-                    Netherland
-                  </span>
-                </span>
-              </p>
-            </div>
           </div>
-        </div>
+        ) : null}
       </div>
     </Layout>
   );
