c4991facb3b18faf0b80e2b8cc00f00f157489aa
EstherSeyi <sheyiogundijo@gmail.com>
2021-04-26 08:30:31 +0100

    modifies tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    enabled: process.env.NODE_ENV !== "development",
+    content: [
+      "./components/**/*.js",
+      "./components/**/*.jsx",
+      "./pages/**/*.js",
+      "./pages/**/*.jsx",
+    ],
+  },
+
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
